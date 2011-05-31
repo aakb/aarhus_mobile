b04ceff31d3b8308262ad31a6077160689d8dbba
Jesper Wøldiche <jesper@woeldiche.dk>
2011-05-31 10:40:16 +0200

    Added collapsible, search, ting-objects

--- template.php
+++ template.php
@@ -1,9 +1,8 @@
 <?php
-
+/**
+ * Implementation of hook_preprocess_page.
+ */ 
 function aarhus_mobile_preprocess_page(&$vars) {
-  global $theme;
-  global $theme_path;
-  
   // Don't display empty help from node_help().
   if ($vars['help'] == "<div class=\"help\"> \n</div>") {
     $vars['help'] = '';
@@ -16,33 +15,4 @@ function aarhus_mobile_preprocess_page(&$vars) {
     $vars['site_logo'] = '<a class="site-logo" href="'. $vars['front_page'] .'" title="'. t('Home page') .'" rel="home"><img src="'. $vars['logo'] .'" alt="'. $vars['logo_alt_text'] .'" /></a>';
   }   
 
-}
-
-/**
- * Implementation of hook_theme.
- */
-function aarhus_mobile_theme() {
-  return array(
-    'user_login' => array(
-      'template' => 'user-login',
-      'arguments' => array('form' => NULL),
-    ),
-    'ting_search_form' => array(
-      'arguments' => array('form' => NULL),
-    ),
-  );
-}
-
-
-/**
- * Theme function that can be used to remove stuff form the search form. The
- * h2 headline can be disabled on the block for current theme.
- */
-function aarhus_mobile_ting_search_form(&$form){
-  global $theme_path;
-  
-  unset($form['example_text']);
-  // $form['submit']['#type'] = 'image_button';
-  // $form['submit']['#src'] = $theme_path .'/images/button-search.png'; 
-  return drupal_render($form);
 }
