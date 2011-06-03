8cfaefbee9ec400e50bd541728e7f8defc5e850b
Jesper Kristensen <cableman@linuxdev.dk>
2011-06-03 10:45:34 +0200

    Fixed overwrite off user login form

--- template.php
+++ template.php
@@ -16,3 +16,23 @@ function aarhus_mobile_preprocess_page(&$vars) {
   }   
 
 }
+
+/**
+ * Implementation of hook_theme.
+ */
+function aarhus_mobile_theme() {
+  return array(
+    'user_login' => array(
+      'arguments' => array('form' => NULL),
+    ),
+  );
+}
+
+/**
+ * Theme function used to change the login box.
+ *
+ * Alternator hiddes some information, that we do not... so we overwrite it here.
+ */
+function aarhus_mobile_user_login($form) {
+  return drupal_render($form);
+}
